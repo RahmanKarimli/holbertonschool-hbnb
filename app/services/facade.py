7cd2d7c0d9e6728c5859c208ccef3d8d0ffffc36
Rahman Karimli <rahman@Rahmans-MacBook-Air.local>
2024-11-12 18:02:28 +0400

    Amenity Endpoints

--- app/services/facade.py
+++ app/services/facade.py
@@ -1,3 +1,4 @@
+from app.models.amenity import Amenity
 from app.models.user import User
 from app.persistence.repository import InMemoryRepository
 
@@ -9,6 +10,7 @@ class HBnBFacade:
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
 
+    # User
     # Placeholder method for creating a user
     def create_user(self, user_data):
         user = User(**user_data)
@@ -31,3 +33,28 @@ class HBnBFacade:
                 setattr(user, key, value)
             self.user_repo.update(user_id, user)
         return user
+
+    # Amenity
+    def create_amenity(self, amenity_data):
+        # Placeholder for logic to create an amenity
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        # Placeholder for logic to retrieve an amenity by ID
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        # Placeholder for logic to retrieve all amenities
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        # Placeholder for logic to update an amenity
+        amenity = self.amenity_repo.get(amenity_id)
+
+        if amenity:
+            for key, value in amenity_data.items():
+                setattr(amenity, key, value)
+            self.amenity_repo.update(amenity_id, amenity)
+        return amenity
